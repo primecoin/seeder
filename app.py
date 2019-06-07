a9ab08ab4b7706e4e5e3bd2e0657ac7e5dcd46a2
Sunny King <sunnyking9999@gmail.com>
2019-06-07 02:50:16 +0000

    api call triggers update of dns seeds on AWS Route53

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, jsonify
 
+import boto3
 import json
 import re
 import requests
@@ -17,6 +18,10 @@ if app.config['NETWORK'] == 'mainnet' and 'MAINNET_RPC_URL' not in app.config:
     raise RuntimeError("Setting 'MAINNET_RPC_URL' is not configured")
 if app.config['NETWORK'] == 'testnet' and 'TESTNET_RPC_URL' not in app.config:
     raise RuntimeError("Setting 'TESTNET_RPC_URL' is not configured")
+if 'ROUTE53_HOSTED_ZONE_ID' not in app.config:
+    raise RuntimeError("Setting 'ROUTE53_HOSTED_ZONE_ID' is not configured")
+if 'ROUTE53_RECORD_NAME' not in app.config:
+    raise RuntimeError("Setting 'ROUTE53_RECORD_NAME' is not configured")
 
 
 def requestJsonRPC(method, params):
@@ -46,7 +51,49 @@ def getServerIPs():
                 pattern = '(?P<host>(\d+\.?){4})(:?P<port>[0-9]*)?'
                 match = re.search(pattern, peer["addr"])
                 servers.append(match.group('host'))
-    response["result"] = servers
+
+    route53 = boto3.client('route53')
+    response["update"] = servers
+
+    # Retrieve existing seeds in dns
+    seedsExisting = []
+    recordSet = route53.list_resource_record_sets(
+        HostedZoneId=app.config['ROUTE53_HOSTED_ZONE_ID'],
+        StartRecordName=app.config['ROUTE53_RECORD_NAME'],
+        StartRecordType='A'
+    )
+    for record in recordSet["ResourceRecordSets"]:
+        seedsExisting.append(record["ResourceRecords"][0]["Value"])
+    response["former"] = seedsExisting
+
+    # Add new seeds to dns
+    seedsNew = []
+    for server in servers:
+        if server not in seedsExisting:
+            seedsNew.append(server)
+            route53.change_resource_record_sets(
+                HostedZoneId=app.config['ROUTE53_HOSTED_ZONE_ID'],
+                ChangeBatch={
+                    'Changes': [
+                        {
+                            'Action': 'CREATE',
+                            'ResourceRecordSet': {
+                                'Name': app.config['ROUTE53_RECORD_NAME'],
+                                'Type': 'A',
+                                'SetIdentifier': server,
+                                'MultiValueAnswer': True,
+                                'TTL': 60,
+                                'ResourceRecords': [
+                                    {
+                                        'Value': server
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            )
+    response["result"] = seedsExisting + seedsNew
     return jsonify(response), 200
 
 
